e11519887a813271ee0a6af6862c1d70c1726aa8
Broderick M. DeSantis <desantisbm@appstate.edu>
2017-01-30 18:59:09 -0500

    update hello on dev branch to allow pull and merge

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! now with dev branch functionality!");
 	}
 }
